78323fe7cf310a0a2b7442696bb4b7e839a804bd
Marcelo Mazzotti Paes de Almeida <m4mazzotti@gmail.com>
2013-04-20 19:28:41 -0400

    Initial Commit

---
+++ Attribute/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Attribute
+//
+//  Created by Marcelo Mazzotti on 20/4/13.
+//  Copyright (c) 2013 Marcelo Mazzotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Attribute/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Attribute
+//
+//  Created by Marcelo Mazzotti on 20/4/13.
+//  Copyright (c) 2013 Marcelo Mazzotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
